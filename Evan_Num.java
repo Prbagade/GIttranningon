8737f5aac25d3aa23975cefbd14f7f77c8d45e00
Prbagade <120325779+Prbagade@users.noreply.github.com>
2023-02-26 23:00:34 +0530

    second commit

---
+++ Evan_Num.java
@@ -0,0 +1,18 @@
+package logical_problam;
+
+public class Evan_Num {
+
+	public static void main(String[] args) 
+	{
+		for(int s=0;s<=10;s++)
+		{
+			if(s%2==0)// for Even Num
+			//if(s%2!=0)// for odd Num
+				//System.out.println("Its Odd num = "+ s);
+				System.out.println("Its Even Num = "+s);
+		}
+		
+	
+	}
+
+}
---
+++ example.java
@@ -0,0 +1,20 @@
+package logical_problam;
+
+public class example {
+
+	public static void main(String[] args)
+	{
+		for(int a=1; a<=20; a++) 
+		{
+			System.out.print(a+" ");
+			
+		}
+		System.out.println("\n+++++++++++++++++++++++++++++++++++++++");
+		
+		for(int s=20; s>=1; s-- ) 
+		{
+			System.out.print(s+" ");
+		}
+	}
+
+}
